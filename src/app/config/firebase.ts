02b10da53be3adb333968b11223eac61333d35ec
VinhKhanh <vinhldk@devblock.net>
2020-12-26 14:26:02 +0700

    Initial codebase component style common function

---
+++ src/app/config/firebase.ts
@@ -0,0 +1,12 @@
+export const firebaseconfig = {
+
+    firebase : {
+        apiKey: "API_KEY",
+        authDomain: "AUTH_DOMAIN",
+        databaseURL: "DATABASE_URL",
+        projectId: "PROJECT_ID",
+        storageBucket: '',
+        messagingSenderId: ''
+      }
+
+};
